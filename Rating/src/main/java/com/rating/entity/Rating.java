d53ed139e0b5a4105ab04a6c9f95fc09dd97006c
Akshay Rane <akshayrane2750@gmail.com>
2026-08-18 22:19:58 +0530

    Added auto increment id annotation

--- Rating/src/main/java/com/rating/entity/Rating.java
+++ Rating/src/main/java/com/rating/entity/Rating.java
@@ -1,6 +1,8 @@
 package com.rating.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -17,6 +19,7 @@ import lombok.Setter;
 public class Rating {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Long userId;
 	private Long hotelId;
