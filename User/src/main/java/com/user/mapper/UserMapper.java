26864838b808f8aa4e706faa3ad44f186194f085
Akshay Rane <akshayrane2750@gmail.com>
2026-08-19 22:45:40 +0530

    Implement user mapper

---
+++ User/src/main/java/com/user/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package com.user.mapper;
+
+import com.user.dto.UserDto;
+import com.user.entity.User;
+
+public class UserMapper {
+
+	public static User mapToUser(UserDto userDto) {
+		User user = new User(
+					userDto.id(),
+					userDto.name(),
+					userDto.email(),
+					userDto.password()
+				);
+		return user;
+	}
+	
+	public static UserDto maptoUserDto(User user){
+		UserDto userDto = new UserDto(
+				user.getId(),
+				user.getName(),
+				user.getEmail(),
+				user.getPassword()
+				);
+		return userDto;
+	}
+}
