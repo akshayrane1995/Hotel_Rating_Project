071f37a08be8a7c08bedcf21dd24ce895f57f2b6
Akshay Rane <akshayrane2750@gmail.com>
2026-08-21 17:36:46 +0530

    Implemented feign client for User service

---
+++ Rating/src/main/java/com/rating/client/UserServiceClient.java
@@ -0,0 +1,14 @@
+package com.rating.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.rating.dto.UserDto;
+
+@FeignClient(name = "user-service", url = "${user.service.url}")
+public interface UserServiceClient {
+
+    @GetMapping("/user/id/{id}")
+    UserDto getUserById(@PathVariable Long id);
+}
