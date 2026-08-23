aaf00ffaa9fc01d39d34bb3647ff35679f90480b
Akshay Rane <akshayrane2750@gmail.com>
2026-08-24 00:06:50 +0530

    Enable feign client

--- Rating/src/main/java/com/rating/RatingApplication.java
+++ Rating/src/main/java/com/rating/RatingApplication.java
@@ -2,8 +2,10 @@ package com.rating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class RatingApplication {
 
 	public static void main(String[] args) {
