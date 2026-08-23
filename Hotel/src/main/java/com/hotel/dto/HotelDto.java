9e952f2e08c73ac0e096986bc80e93e326fc0df3
Akshay Rane <akshayrane2750@gmail.com>
2026-08-23 23:44:14 +0530

    Validation added

--- Hotel/src/main/java/com/hotel/dto/HotelDto.java
+++ Hotel/src/main/java/com/hotel/dto/HotelDto.java
@@ -1,5 +1,13 @@
 package com.hotel.dto;
 
-public record HotelDto(Long id, String name, Long rating, String city) {
-    
+import jakarta.validation.constraints.NotBlank;
+
+public record HotelDto(Long id, 
+						
+						@NotBlank(message = "Hotel name is required") 
+						String name,
+						
+						@NotBlank(message = "City is required")
+						String city) {
+
 }
