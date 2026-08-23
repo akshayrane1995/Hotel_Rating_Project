0f9dc32dc4e81995c16d9e0b40a21fc4474b5c38
Akshay Rane <akshayrane2750@gmail.com>
2026-08-23 23:55:36 +0530

    Validation added

--- Rating/src/main/java/com/rating/dto/RatingDto.java
+++ Rating/src/main/java/com/rating/dto/RatingDto.java
@@ -1,5 +1,24 @@
 package com.rating.dto;
 
-public record RatingDto(Long id, Long userId, Long hotelId, Long rating, String feedback) {
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RatingDto(Long id, 
+						
+						@NotNull(message = "User id is required")
+						Long userId, 
+						
+						@NotNull(message = "Hotel id is required")
+						Long hotelId, 
+						
+						@NotNull(message = "Rating is required")
+				        @Min(value = 1, message = "Rating must be at least 1")
+				        @Max(value = 5, message = "Rating must not exceed 5")
+						Long rating, 
+						
+						@NotBlank(message = "Feedback is required")
+						String feedback) {
 
 }
