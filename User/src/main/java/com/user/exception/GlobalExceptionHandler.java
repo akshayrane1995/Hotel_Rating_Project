7bd07fbbe704222e45a05632b4d3b5b162dddf79
Akshay Rane <akshayrane2750@gmail.com>
2026-08-23 23:52:26 +0530

    Implemented handler for validation

--- User/src/main/java/com/user/exception/GlobalExceptionHandler.java
+++ User/src/main/java/com/user/exception/GlobalExceptionHandler.java
@@ -1,38 +1,50 @@
 package com.user.exception;
+
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(
-            ResourceNotFoundException exception,
-            WebRequest webRequest) {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
+			WebRequest webRequest) {
 
-        ErrorDetails errorDetails = new ErrorDetails(
-                LocalDateTime.now(),
-                exception.getMessage(),
-                webRequest.getDescription(false),
-                "USER_RESOURCE_NOT_FOUND"
-        );
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), exception.getMessage(),
+				webRequest.getDescription(false), "USER_RESOURCE_NOT_FOUND");
 
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-    }
-    
-    // handle generic exception
- 	@ExceptionHandler(Exception.class)
- 	public ResponseEntity<ErrorDetails> handleGenericException(Exception exception, WebRequest webRequest) {
- 		ErrorDetails errorDetails = new ErrorDetails(
- 				LocalDateTime.now(), 
- 				exception.getMessage(),
- 				webRequest.getDescription(false), "Internal Server error");
+		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+	}
 
- 		return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
- 	}
+	// handle generic exception
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorDetails> handleGenericException(Exception exception, WebRequest webRequest) {
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), exception.getMessage(),
+				webRequest.getDescription(false), "Internal Server error");
+
+		return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorDetails> handleValidationException(MethodArgumentNotValidException exception,
+			WebRequest webRequest) {
+
+		String message = exception.getBindingResult()
+				.getFieldErrors()
+				.stream()
+				.map(error -> error.getField() + ": " + error.getDefaultMessage())
+				.collect(Collectors.joining(", "));
+
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), message, webRequest.getDescription(false),
+				"VALIDATION_ERROR");
+
+		return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+	}
 }
