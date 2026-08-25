0da4b2da3c886773fcf9a737fa42740aa6fa0f21
Akshay Rane <akshayrane2750@gmail.com>
2026-08-25 23:28:39 +0530

    remove validation from this class

--- User/src/main/java/com/user/dto/UserDto.java
+++ User/src/main/java/com/user/dto/UserDto.java
@@ -1,15 +1,3 @@
 package com.user.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-public record UserDto(Long id, 
-					@NotBlank(message = "Name is required")
-					String name,
-					
-
-			        @NotBlank(message = "Email is required")
-			        @Email(message = "Please enter a valid email")
-					String email) {
-
-}
+public record UserDto(Long id, String name, String email) {}
