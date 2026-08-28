028785f9c76235f57ddf00ae7200fbd1ba1f47af
Akshay Rane <akshayrane2750@gmail.com>
2026-08-28 23:02:12 +0530

    Implemented feign client config for forward jwt token from user service

---
+++ Rating/src/main/java/com/rating/client/FeignClientConfig.java
@@ -0,0 +1,31 @@
+package com.rating.client;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import feign.RequestInterceptor;
+
+@Configuration
+public class FeignClientConfig {
+
+	@Bean
+	public RequestInterceptor requestInterceptor() {
+
+		return requestTemplate -> {
+
+			ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder
+					.getRequestAttributes();
+
+			if (attributes != null) {
+
+				String authorization = attributes.getRequest().getHeader("Authorization");
+
+				if (authorization != null) {
+					requestTemplate.header("Authorization", authorization);
+				}
+			}
+		};
+	}
+}
